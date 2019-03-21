45b8549f958286776ac49e83c49b0dee56c047de
nrj101 <acc.neerajtripathi@gmail.com>
2019-03-21 20:05:03 +0530

    Final

--- Binary_Search_Tree.cpp
+++ Binary_Search_Tree.cpp
@@ -11,9 +11,13 @@ struct node{
 
 node* Create(int data);
 node* Insert(node* root,int data);
-void Display(node* root);
-int min(node* root);
 node* Delete(node* root,int data);
+int max(node* root);
+int min(node* root);
+int height(node* root);
+bool Search(node* root, int data);
+void InOrderDisplay(node* root);
+void LevelOrderDisplay(node* root);
 
 int main()
 {
@@ -21,10 +25,11 @@ int main()
 	int n,r;
 
 	while(1){
-	   cout<<"\n\nPress : \n 1 to insert a new node\n 2 to delete an node\n 3 to display all the nodes\n 4 to quit";
+	   cout<<"\n\nPress : \n 1 to insert a new node\n 2 to delete an node\n 3 to display nodes in IN-ORDER\n 4 to display nodes in  LEVEL-ORDER";
+	   cout<<"\n 5 to display root node, height, max. and min. nodes of tree\n 6 to Search for a node\n 7 to quit"<<endl;
        cout<<"\nYour Response : ";
 	   cin>>r;
-	   if( (root==NULL)  &&  (r==2||r==3) )
+	   if( (root==NULL) && (  (r==2)||(r==3)||(r==4)||(r==5)||(r==6)  ) )
 	   {
 		   cout<<"\nError !   Tree is empty. Insert some nodes first"<<endl;
 		   continue ;
@@ -40,10 +45,25 @@ int main()
 				   root=Delete(root,n);
 			       break;
 
-		   case 3: Display(root);
+		   case 3: InOrderDisplay(root);
 				   break;
 
-		   case 4: exit(0);
+		   case 4: LevelOrderDisplay(root);
+				   break;
+
+		   case 5: 	cout<<"\n\t Root = "<<root->info;
+					cout<<"\n\t Height of Tree = "<<height(root);
+					cout<<"\n\t Max. Value = "<<max(root);
+					cout<<"\n\t Min. Value = "<<min(root);
+				   break;
+
+		   case 6: cout<<"\n\tEnter a number to search :  ";
+				   cin>>n;
+				   if(Search(root,n))
+						cout<<"\n\tFound";
+				   else
+						cout<<"\n\tNot Found";
+		   case 7: exit(0);
 
 		   default: cout<<"\nInvalid entry"<<endl;
 	    }
@@ -53,13 +73,59 @@ int main()
 	return 0;
 }
 
-node* Create(int data)
+void LevelOrderDisplay(node* root)
 {
-	node* newNode=new node();
-	newNode->info=data;
-	newNode->lc=NULL;
-	newNode->rc=NULL;
-	return newNode;
+	if(root==NULL)
+		return;
+	queue<node*> q;
+	q.push(root);
+	while(!q.empty())
+	{
+		node* current=q.front();
+		cout<<"\t"<<current->info;
+		if(current->lc!=NULL)q.push(current->lc);
+		if(current->rc!=NULL)q.push(current->rc);
+		q.pop();
+	}
+	return;
+}
+
+void InOrderDisplay(node* root)
+{
+	if(root==NULL)
+		return;
+	InOrderDisplay(root->lc);
+	cout<<"\t"<<root->info;
+	InOrderDisplay(root->rc);
+}
+
+int height(node* root)
+{
+	if(root==NULL)
+		return -1;
+	else
+	{
+		int l=height(root->lc);
+		int r=height(root->rc);
+		if(l>r)
+			return l+1;
+		else return r+1;
+	}
+}
+
+int max(node* root)
+{
+	if(root->rc==NULL)
+		return root->info;
+	else return max(root->rc);
+
+}
+int min(node* root)
+{
+	if(root->lc==NULL)
+		return root->info;
+	else return min(root->lc);
+
 }
 
 node* Insert(node* root,int data)
@@ -75,22 +141,7 @@ node* Insert(node* root,int data)
 	return root;
 }
 
-void Display(node* root)
-{
-	if(root==NULL)
-		return;
-	Display(root->lc);
-	cout<<"\t"<<root->info;
-	Display(root->rc);
-}
 
-int min(node* root)
-{
-	if(root->lc==NULL)
-		return root->info;
-	else return min(root->lc);
-
-}
 
 node* Delete(node* root,int data)
 {
@@ -130,4 +181,27 @@ node* Delete(node* root,int data)
 	}
 
 	return root;
+}
+
+
+
+bool Search(node* root,int data)
+{
+	if(root==NULL)
+		return false;
+	else if(root->info==data)
+		return true;
+	else if(root->info>=data)
+		return Search(root->lc,data);
+	else
+		return Search(root->rc,data);
+}
+
+node* Create(int data)
+{
+	node* newNode=new node();
+	newNode->info=data;
+	newNode->lc=NULL;
+	newNode->rc=NULL;
+	return newNode;
 }
